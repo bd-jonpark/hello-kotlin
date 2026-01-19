a1051b547747b2a77b86004f564b95aee42403c8
JongYong Park <jonpark@blackduck.com>
2026-01-19 01:10:58 -0500

    Update to Kotlin 2.0.0 with JDK 21 toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.0.0"
     application
 }
 
@@ -8,6 +8,10 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
